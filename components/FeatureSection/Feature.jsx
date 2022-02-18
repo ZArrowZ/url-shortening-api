226bc3232eada4ad47321f8126ba7f8e38438236
Ziad Said <ziads3232@gmail.com>
2022-02-18 23:11:06 +0200

    add with to features images

--- components/FeatureSection/Feature.jsx
+++ components/FeatureSection/Feature.jsx
@@ -1,5 +1,6 @@
+import Image from "next/image";
 /* eslint-disable @next/next/no-img-element */
-const Feature = ({ img, alt, h2, p }) => {
+const Feature = ({ img, alt, h2, p, width, height }) => {
   return (
     <div
       className="text-center md:text-left md:relative md:first:top-0 md:top-[44px] md:last:top-[88px]  flex flex-col items-center md:items-start px-4 md:px-[33px] pb-8 
@@ -9,7 +10,7 @@ const Feature = ({ img, alt, h2, p }) => {
         className="bg-[hsl(257,27%,26%)] w-[88px] h-[88px] rounded-full flex items-center justify-center
         relative top-[-44px]"
       >
-        <img src={img} alt={alt} />
+        <Image src={img} alt={alt} width={width} height={height} />
       </div>
       <h2 className="text-[hsl(255,11%,22%)] font-bold relative top-[-16px] mt-[6px] text-[20px]">
         {h2}
--- components/FeatureSection/Features.jsx
+++ components/FeatureSection/Features.jsx
@@ -8,6 +8,8 @@ const Features = () => {
         img="/icon-brand-recognition.svg"
         alt="icon brand recognition"
         h2="Brand Recognition"
+        width={40}
+        height={40}
         p="Boost your brand recognition with each click Generic links don't mean a thing.
         Branded links help instil confidence in your content."
       />
@@ -16,6 +18,8 @@ const Features = () => {
         img="/icon-detailed-records.svg"
         alt="icon Detailed Records"
         h2="Detailed Records"
+        width={40}
+        height={40}
         p="Gain insights into who is clicking your links. Knowing when and where
         people engage with your content help inform better decisions."
       />
@@ -24,6 +28,8 @@ const Features = () => {
         img="/icon-fully-customizable.svg"
         alt="icon Fully Customizable"
         h2="Fully Customizable"
+        width={48}
+        height={48}
         p="Improve brand awareness and content discoverability through customizable
         links, supercharging audience engagement."
       />
