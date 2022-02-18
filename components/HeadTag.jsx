7dd9683189461976604b01c8dbf9925c6317d5dc
Ziad Said <ziads3232@gmail.com>
2022-02-18 23:33:15 +0200

    add description to meta tag

--- components/HeadTag.jsx
+++ components/HeadTag.jsx
@@ -4,6 +4,10 @@ const HeadTag = () => {
   return (
     <Head>
       <title>Shortly</title>
+      <meta
+        name="description"
+        content="Link shorteners work by transforming any long URL into a shorter and make it more readable."
+      ></meta>
     </Head>
   );
 };
