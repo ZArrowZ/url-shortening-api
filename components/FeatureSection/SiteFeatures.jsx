da6f34391d5203c4e95fff7d885dfa2e24cca1d5
Ziad Said <ziads3232@gmail.com>
2022-02-20 00:14:10 +0200

    change headline from h1 to h2

--- components/FeatureSection/SiteFeatures.jsx
+++ components/FeatureSection/SiteFeatures.jsx
@@ -4,9 +4,9 @@ const SiteFeatures = () => {
   return (
     <section className="px-[24px]">
       <div className="max-w-[515px] mx-auto py-[0.5rem] md:py-[2.9rem]">
-        <h1 className="text-[1.60rem] md:text-[2.3rem] font-bold text-[hsl(255,11%,22%)] text-center my-4">
+        <h2 className="text-[1.60rem] md:text-[2.3rem] font-bold text-[hsl(255,11%,22%)] text-center my-4">
           Advanced Statistics
-        </h1>
+        </h2>
         <p className="text-[#A4A3A6]  text-center md:text-[18px]">
           Track how your links are performing across the web with our advanced
           statistics dashboard
